0398d2ea9e7d1535c217fc0bd112dcf9b6dff19a
Shikhar Verma <shikharv2@protonmail.com>
2025-03-09 00:41:23 -0500

    made a placeholder modal

--- react-frontend/src/components/Form/index.jsx
+++ react-frontend/src/components/Form/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 function Form(){
+    const [modal,setModal]=useState(false); /*sets the modal intially to false*/
+    const toggleModal=()=>{ /*when the function is called it sets the modal to the opposite of its current state */
+        setModal(!modal)
+    }
     return(
     <>
         <div class="search-bar">
             <input type="search" class="search-input"></input>
         </div>
-        <button class="addButton">+</button>
+        <button class="addButton" onClick={toggleModal}>+</button> {/*when the button is pressed it will run the function toggleModal*/}
+        {modal && ( /* is the same as using an if statement to see if modal is equal to true. && is a logical operator which evaluates the left hand side to see if it is true. 
+        if modal is true then it will execute the code below. if modal is false then the right hand side will also be false and not run the code*/
+            <div class="modal">
+                <div class="overlay">
+                    <div class="modal-content">
+                        <h2>Hello Modal</h2>
+                        <p>placeholder text is super duper fun to make and write.</p>
+                        <button class="close-button" onClick={toggleModal}>close</button>
+                    </div>
+                </div>
+            </div>
+        )}
+        
     </>
     );
 }
